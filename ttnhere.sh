6585095808ef707faf37fd8021afca981e850235
wryandginger <77764889+wryandginger@users.noreply.github.com>
2026-07-12 12:05:54 -0700

    Update ttnhere.sh
    fixes for docker

--- ttnhere.sh
+++ ttnhere.sh
@@ -4,18 +4,24 @@
 USER_HOME="/app"
 PYTHON_BIN="/usr/bin/python3"
 
-# Navigate to home directory
+# Navigate to the defined home directory
 cd "$USER_HOME"
 
 # Infinite loop to keep the script restarting
 while true; do
+    # Takes control over the SDR and holds for ready 
+    usbreset RTL2838UHIDIR
+    sleep 2
+    
     # Run the first pair of scripts (ignores failures to prevent loop stoppage)
     $PYTHON_BIN "$USER_HOME/ttn.py" || true
     $PYTHON_BIN "$USER_HOME/gif_ttn.py" || true
 
-    # Pause for 1 second
-    sleep 1
-
+    # Take a minute break, then take control over the SDR and holds for ready 
+    sleep 65
+    usbreset RTL2838UHIDIR
+    sleep 2
+    
     # Run the second pair of scripts
     $PYTHON_BIN "$USER_HOME/here.py" || true
     $PYTHON_BIN "$USER_HOME/gif_here.py" || true
